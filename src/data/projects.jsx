0331f917766c8b40a9870ca04d23b5d5fc3a67d8
kevinxmyr <kevinpunongbayan@gmail.com>
2023-02-01 04:05:06 +0800

    finish

--- src/data/projects.jsx
+++ src/data/projects.jsx
@@ -6,20 +6,6 @@ import kidney from '../assets/projects-assets/kidney.jpeg'
 
 export const projects = [
    {
-      projectTitle: 'FaceBot',
-      description: 'A simple ReactJS app with Search filter functionality, updates the display to show filtered names. Using an API to fetch data. Check it out!',
-      image: facebot,
-      tech: '#React #TachyonsCSS',
-      projUrl: 'https://kevinxmyr.github.io/Facebot/'
-   },
-   {
-      projectTitle: 'Background Generator',
-      description: 'Background generator lets you pick the color gradient. This project is coded in using HTML, CSS and JavaScript. Plus it shows the color code in RGB! try it out!',
-      image: bggenerator,
-      tech: '#HTML #CSS #JavaScript',
-      projUrl: 'https://kevinxmyr.github.io/BackgroundGenerator/'
-   },
-   {
       projectTitle: 'Fluff Koppi Cafe',
       description: 'The design of this website was created using Tailwind CSS and React Vite. The site was designed to be responsive, so it looks great on both desktop and mobile devices.',
       image: fluffimage,
@@ -34,7 +20,21 @@ export const projects = [
       projUrl: 'http://react-movie-search-seven.vercel.app/'
    },
    {
-      projectTitle: `National Kidney Foundation (COPY)`,
+      projectTitle: 'FaceBot',
+      description: 'A simple ReactJS app with Search filter functionality, updates the display to show filtered names. Using an API to fetch data. Check it out!',
+      image: facebot,
+      tech: '#React #TachyonsCSS',
+      projUrl: 'https://kevinxmyr.github.io/Facebot/'
+   },
+   {
+      projectTitle: 'Background Generator',
+      description: 'Background generator lets you pick the color gradient. This project is coded in using HTML, CSS and JavaScript. Plus it shows the color code in RGB! try it out!',
+      image: bggenerator,
+      tech: '#HTML #CSS #JavaScript',
+      projUrl: 'https://kevinxmyr.github.io/BackgroundGenerator/'
+   },
+   {
+      projectTitle: `National Kidney Foundation (Availbale on Mobile View Only, copy website)`,
       description: `This is a copy of the website kidney.org, which I found on the Internet. I was hoping to test my knowledge of tailwind CSS, and this is a perfect example of what I'm talking about. See live preview below.`,
       image: kidney,
       tech: '#React #TailwindCSS',
